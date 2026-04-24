c162192c8513d04b75f12039a3063203abd691ab
MuriloJamoski <mugames2002@gmail.com>
2026-04-24 10:08:58 -0300

    feat: criação da entidade para Livro

---
+++ core/serializers/livro.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Livro
+
+class LivroSerializer(ModelSerializer):
+    class meta:
+        model = Livro
+        fields = '__all__'
